fea0b2197b9b2b4c48bb7a4a47cb8ab1fa4fabd4
David <david.yu@readify.net>
2020-10-17 11:20:30 +0800

    Updates

---
+++ precache-manifest.b955f6ca7ceb3bcb3a143f49b1db29fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51dcb4fe95c82f7d51822c08754bce8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c08f8fd94f198bb5baf",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "3ea9e82bd33571586904",
+    "url": "./static/js/2.ab7af02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ab7af02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c08f8fd94f198bb5baf",
+    "url": "./static/js/main.78a3c294.chunk.js"
+  },
+  {
+    "revision": "6899eb1ddc3a8b0ddce5",
+    "url": "./static/js/runtime-main.7cedc0a8.js"
+  }
+]);
